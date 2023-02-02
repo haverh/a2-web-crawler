da9ef7400df4e16c8f8d69f4000d0c92e3b32cd9
Haver D. Ho <haverh@circinus-1.ics.uci.edu>
2023-02-01 16:32:45 -0800

    Modified scraper.py ( Added Defragmentation )

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import pickle
 from collections import defaultdict
 from os.path import exists
+from sys import getsizeof
 
 def scraper(url, resp):
 	links = extract_next_links(url, resp)
@@ -23,48 +24,61 @@ def extract_next_links(url, resp):
 	domains = [".ics.uci.edu", ".cs.uci.edu", ".informatics.uci.edu", ".stat.uci.edu"]
 	crawledLinks = []
 	# print("NETLOC : " + urlparse(url).netloc)
-	
 
 	if  (resp.status == 200):
-		#print("STATUS : " + str(resp.status))
+
+		sz = getsizeof(resp.raw_response.content)
+
+		if (sz >= 10000 and sz < 1000000):
+			#print("STATUS : " + str(resp.status))
+			soup = BeautifulSoup( resp.raw_response.content, "html.parser")
+
+
+			# Get all urls that are in the a valid domain
+			for link in soup.find_all('a'):
+				linkUrl = link.get('href')
+
+				if ( linkUrl ):
+					parsed = urlparse(linkUrl)
+					
+					if ( len(parsed.fragment) > 1 ):
+						#print("OLD: " + linkUrl)
+						#print("FRAGMENT === " + "#" + parsed.fragment)
+						linkUrl = linkUrl.replace('#'+parsed.fragment, '')
+						#print("NEW: " + linkUrl)
+
+					# Create url with a page path
+					isPath = re.match("^\/\w+", linkUrl)
+					if ( isPath ):
+						newUrl = urlparse(url).netloc + linkUrl
+						# print("ADDED -> " + str(newUrl))
+						crawledLinks.append(newUrl)
 		
-		soup = BeautifulSoup( resp.raw_response.content, "html.parser")
-
-		# Get all urls that are in the a valid domain
-		for link in soup.find_all('a'):
-			linkUrl = link.get('href')
-
-			if ( linkUrl ):
-				# Create url with a page path
-				isPath = re.match("^\/\w+", linkUrl)
-				if ( isPath ):
-					newUrl = urlparse(url).netloc + linkUrl
-					# print("ADDED -> " + str(newUrl))
-					crawledLinks.append(newUrl)
+					else:
+						# Check if domain name is valid
+						for domain in domains:
+							if ( domain in urlparse(linkUrl).netloc ):
+								# print("ADDED -> " + str(linkUrl))
+								crawledLinks.append(linkUrl)
 		
-				else:
-					# Check if domain name is valid
-					for domain in domains:
-						if ( domain in linkUrl ):
-							# print("ADDED -> " + str(linkUrl))
-							crawledLinks.append(linkUrl)
-	
-		if ( not exists('validLinks.bin') ):
-			vLinks = open( 'validLinks.bin', 'wb' )
-			validLinks = {resp.url}
-			pickle.dump(validLinks, vLinks)
-			vLinks.close()
-		else:
-			vLinks = open('validLinks.bin', 'rb')
-			validLinks = pickle.load(vLinks)
-			validLinks.add(resp.url)
-			vLinks.close()
+			if ( not exists('validLinks.bin') ):
+				vLinks = open( 'validLinks.bin', 'wb' )
+				validLinks = {resp.url}
 
-			vLinks = open('validLinks.bin', 'wb')
-			pickle.dump(validLinks, vLinks)
-			vLinks.close
+				pickle.dump(validLinks, vLinks)
+				vLinks.close()
+			else:
+				vLinks = open('validLinks.bin', 'rb')
+				validLinks = pickle.load(vLinks)
+				validLinks.add(resp.url)
+				vLinks.close()
+
+				vLinks = open('validLinks.bin', 'wb')
+				pickle.dump(validLinks, vLinks)
+				vLinks.close
 	
-		return crawledLinks
+			return crawledLinks
+		return list()
 
 
 
@@ -86,7 +100,19 @@ def is_valid(url):
 		
 		if parsed.scheme not in set(["http", "https"]):
 			return False
-		
+
+		if ( re.match(
+			r".*\.(css|js|bmp|gif|jpe?g|ico"
+			+ r"|png|tiff?|mid|mp2|mp3|mp4"
+			+ r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+			+ r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+			+ r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+			+ r"|epub|dll|cnf|tgz|sha1"
+			+ r"|thmx|mso|arff|rtf|jar|csv"
+			+ r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())):
+			return False;
+
+
 		pToken = parsed.path.split('/')
 		
 		for word in pToken:
